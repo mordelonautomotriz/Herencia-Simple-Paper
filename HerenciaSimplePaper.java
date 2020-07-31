e04e98188219543f935db58ea9f9eedc7241ffbe
mordelonautomotriz <65460749+mordelonautomotriz@users.noreply.github.com>
2020-07-31 17:15:41 -0500

    Algoritmo Herencia Simple

---
+++ HerenciaSimplePaper.java
@@ -0,0 +1,87 @@
+/*
+PROBLEMATICA:
+A nivel conceptual, comportamientos erróneos de casos de esquina
+en software basado en DNN son análogos a errores lógicos en software tradicional.
+Es un problema desafiante, como se señaló
+por grandes compañías de software como Google y Tesla que ya han
+desplegó técnicas de aprendizaje automático en varios sistemas a escala de producción
+incluidos automóviles sin conductor, reconocimiento de voz, búsqueda de imágenes,
+etc.
+
+Herencia simple de accidentes del mundo real que involucran autos autónomos
+ */
+package herenciasimplepaper;
+
+class Empresa{
+    long fecha_accidente;
+    String causa_accidente;
+    String consecuencia_accidente;
+    
+    
+ void MostrarCaracteristicasAccidente(){
+     System.out.println("el accidente sucedio el : " + fecha_accidente + " "
+             + "\n\nla causa del accidente fue la: " + causa_accidente + " y tuvo como "
+                     + "\n consecuencia que: " +consecuencia_accidente);
+ }
+}
+//HERENCIA SIMPLE: una subclase de Empresa es Hyundai 
+class Hyundai extends Empresa{
+String tipo;
+
+String ExponerAccidente(){
+    return "Los sensores no pudieron detectar los letreros de las calles las "
+            + "\n\t\t\t\t\t\tmarcas de los carriles e incluso los peatones"
+            + "\n\t\t\t\t\t\tdebido al angulo del automovil";
+}
+void IndicarTipo(){
+    System.out.println("Esta empresa es de tipo : " + tipo);
+}
+}
+
+//herencia simple: una subclase de Empresa es Tesla
+class Tesla extends Empresa{
+String tipo;
+
+String ExponerAccidente(){
+    return "La camara no pude reconcer el camion blanco contra"
+            + "\n\t\t\t\t\t\tun cielo brillante";
+}
+void IndicarTipo(){
+    System.out.println("Esta empresa es de tipo : " + tipo);
+}
+}
+
+public class HerenciaSimplePaper {
+
+    public static void main(String[] args) {
+        Hyundai hyundai1 = new Hyundai();
+        Tesla tesla1= new Tesla();
+        
+        hyundai1.fecha_accidente=122014;
+        hyundai1.causa_accidente="caida de lluvia";
+        hyundai1.consecuencia_accidente="se estreello durante la prueva\n";
+        hyundai1.tipo="Automotriz";
+        
+        tesla1.fecha_accidente=72016;
+        tesla1.causa_accidente="resolucion en el contraste de la imagen";
+        tesla1.consecuencia_accidente="provoco la muerte del conductor\n";
+        tesla1.tipo="Electrica-Automotriz";
+        
+    System.out.println("\t\tUniversidad de las Fuerzas Armadas Espe - Sede Latacunga");
+    System.out.println("Materia: Programacion");
+    System.out.println("NRC: 7450");
+    System.out.println("Docente: Ing. Luis Guerra");
+    System.out.println("Nombre: Ortiz Hector\n");
+    
+        System.out.println("Informacion de la empresa Hyundai");
+        hyundai1.IndicarTipo();
+        hyundai1.MostrarCaracteristicasAccidente();
+        System.out.println("Como resultado definieron lo siguiente que: "+hyundai1.ExponerAccidente());
+        
+        System.out.println("\n\nInformacion de la empresa Tesla");
+        tesla1.IndicarTipo();
+        tesla1.MostrarCaracteristicasAccidente();
+        System.out.println("Como resultado definieron lo siguiente que: "+tesla1.ExponerAccidente());
+    }
+    
+}
